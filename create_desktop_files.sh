84556e8ed3cfd4f48e61f98dec7f66ef71302500
actionschnitzel <actionschnitzel@guideos.de>
2025-03-23 11:49:33 +0100

    First Push

---
+++ create_desktop_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Sicherstellen, dass die Verzeichnisse existieren
+mkdir -p debian/guideos-browser-mail-backup/usr/share/applications
+
+# Erstellen der ersten .desktop-Datei
+cat > debian/guideos-browser-mail-backup/usr/share/applications/guideos-browser-mail-backup.desktop <<EOL
+[Desktop Entry]
+Version=1.0
+Name=Firefox Backup Tool
+Comment=Sichern und Wiederherstellung von Browser-Profilen und Mail-Daten
+GenericName=Browser Mail Backup
+Exec=guideos-browser-mail-backup
+Icon=guideos-browser-mail-backup
+Terminal=false
+Type=Application
+Categories=GuideOS;
+StartupNotify=true
+EOL
